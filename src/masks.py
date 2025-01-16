52f8feba223e51b6d6e0af4dbbe673d81d2df7bd
Veronika Sidorova <veronikamartyn2106@yandex.ru>
2025-01-16 13:52:13 +0300

    homework1

---
+++ src/masks.py
@@ -0,0 +1,25 @@
+from typing import Union
+
+
+def get_mask_card_number(card_number: Union[int, str]) -> str:
+    """функция, которая принимает на вход номер карты и возвращает ее маску"""
+    card_number_for_mask = str(card_number)
+    if len(card_number_for_mask) == 16 and card_number_for_mask.isdigit() == True:
+        return f"{card_number_for_mask[0:4]} {card_number_for_mask[4:6]}** **** {card_number_for_mask[-4:]}"
+    else:
+        return "Введены некорректные данные!"
+
+
+def get_mask_account(account_number: Union[int, str]) -> str:
+    """функция, которая принимает на вход номер счета и возвращает его маску"""
+    account_number_for_mask = str(account_number)
+    if len(account_number_for_mask) == 20 and account_number_for_mask.isdigit() == True:
+        return f"**{account_number_for_mask[-4:]}"
+    else:
+        return "Введены некорректные данные!"
+
+
+print("Введите номер карты: ")
+print(get_mask_card_number(card_number=input()))
+print("Введите номер счета: ")
+print(get_mask_account(account_number=input()))
