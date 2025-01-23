824dcfaa0f22f63ec87639561f9acba15f9007d9
Veronika Sidorova <veronikamartyn2106@yandex.ru>
2025-01-23 16:27:02 +0300

    start work

--- src/widget.py
+++ src/widget.py
@@ -3,15 +3,17 @@ from typing import Union
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(user_number: Union[int, str]) -> str:
+def mask_account_card(user_number: Union[str]) -> str:
     """Функция, которая замаскирует счет ИЛИ карту"""
-    if "Счет " in str(user_number):
-        return get_mask_account(user_number)
+    if user_number.lower().startswith('счет'):
+        account_number = user_number[-20:]
+        return f"{user_number[:-20]}{get_mask_account(account_number)}"
     else:
-        return get_mask_card_number(user_number)
+        card_number = user_number[-16:]
+        return f"{user_number[:-16]}{get_mask_card_number(card_number)}"
 
 
-def get_date(user_date: Union[int, str]) -> str:
+def get_date(user_date: Union[str]) -> str:
     """Функция, которая выводит дату из строки"""
     user_date_update = str(user_date)
     return f"{user_date_update[8:10]}.{user_date_update[5:7]}.{user_date_update[:4]}"
